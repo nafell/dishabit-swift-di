afc3b46b0f34d24bd67360999f5260f3083bbd10
nafell <pixel68.43@gmail.com>
2025-03-10 11:20:29 +0900

    Created AppDataService: Parent service that manages all data throughout the app.

--- testforkasiwabara/ViewModel/AppService.swift
+++ testforkasiwabara/ViewModel/AppService.swift
@@ -1,19 +1,62 @@
 import Foundation
 import Combine
 
-protocol AppServiceProtocol {
-    var activeQuestsPublisher: AnyPublisher<[Quest], Never> {get}
+protocol AppDataServiceProtocol {
+    var activeQuestsPubisher: AnyPublisher<[Quest], Never> { get }
+    var tasksPubisher: AnyPublisher<[Task], Never> { get }
+    var objectivesPubisher: AnyPublisher<[Objective], Never> { get }
+    var dailyQuestBoardPubisher: AnyPublisher<[DailyQuestBoard], Never> { get }
 }
 
-class AppService: AppServiceProtocol {
-    // CurrentValueSubjectを使用して値を保持し、パブリッシャーとして公開
+class AppDataService: AppDataServiceProtocol {
     private let activeQuestsSubject = CurrentValueSubject<[Quest], Never>([])
+    private let tasksSubject = CurrentValueSubject<[Task], Never>([])
+    private let objectivesSubject = CurrentValueSubject<[Objective], Never>([])
+    private let dailyQuestBoardSubject = CurrentValueSubject<[DailyQuestBoard], Never>([])
+
+    // ==== Public publishers of lists ====
+    var activeQuestsPubisher: AnyPublisher<[Quest], Never> { activeQuestsSubject.eraseToAnyPublisher() }
+    var tasksPubisher: AnyPublisher<[Task], Never> { tasksSubject.eraseToAnyPublisher() }
+    var objectivesPubisher: AnyPublisher<[Objective], Never> { objectivesSubject.eraseToAnyPublisher() }
+    var dailyQuestBoardPubisher: AnyPublisher<[DailyQuestBoard], Never> { dailyQuestBoardSubject.eraseToAnyPublisher() }
     
-    var activeQuestsPublisher: AnyPublisher<[Quest], Never> {
-        return activeQuestsSubject.eraseToAnyPublisher()
+    init () {
+        
     }
-    
+
+    // ==== Publishers for single items for each list ====
+    func activeQuestPublisher(for id: UUID) ->  AnyPublisher<Quest?, Never> {
+        activeQuestsSubject
+            .map { quests in
+                quests.first { $0.id == id }
+            }
+            .eraseToAnyPublisher()
+    }
+    func taskPublisher(for id: UUID) ->  AnyPublisher<Task?, Never> {
+        tasksSubject
+            .map { tasks in
+                tasks.first { $0.id == id }
+            }
+            .eraseToAnyPublisher()
+    }
+    func objectivePublisher(for id: UUID) ->  AnyPublisher<Objective?, Never> {
+        objectivesSubject
+            .map { objectives in
+                objectives.first { $0.id == id }
+            }
+            .eraseToAnyPublisher()
+    }
+    func dailyQuestBoardPublisher(for id: UUID) ->  AnyPublisher<DailyQuestBoard?, Never> {
+        dailyQuestBoardSubject
+            .map { dailyQuestBoards in
+                dailyQuestBoards.first { $0.id == id }
+            }
+            .eraseToAnyPublisher()
+    }
+
     init() {
-        // 初期化処理
+        
     }
+    
+
 }
